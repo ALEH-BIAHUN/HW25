276fbeef8575b2aab18e2e6db738b9580304fd17
Aleh Biahun <stones2875@gmail.com>
2024-06-27 21:07:18 +0200

    Solution task number 25

--- HW25/src/Main.java
+++ HW25/src/Main.java
@@ -1,4 +1,31 @@
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
+
+        Supplier<Man> manSupplier = () -> {
+            System.out.println("Enter your name");
+            String name = new Scanner(System.in).nextLine();
+
+            System.out.println("Enter your age");
+            int age = new Scanner(System.in).nextInt();
+
+            System.out.println("Enter your height");
+            int height = (new Scanner(System.in).nextInt());
+
+            return new Man(name, age, height);
+        };
+        Man man = manSupplier.get();
+        Man woman = manSupplier.get();
+
+        System.out.println(man);
+        System.out.println(woman);
+
+        Predicate<Integer> isAdult = (age) -> age >= 18;
+
+        System.out.println(man.getName() + " adult " + isAdult.test(man.getAge()));
+        System.out.println(woman.getName() + " adult " + isAdult.test(woman.getAge()));
     }
 }
